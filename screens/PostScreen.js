7e632dc84b5d3565b2d49764cc9a9ebdd562f817
aekbote <88759575+aekbote@users.noreply.github.com>
2022-11-26 16:33:40 -0800

    Update PostScreen.js

--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -1,17 +1,114 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+mport React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ScrollView,
+  Dimensions
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import * as Speech from "expo-speech";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
 
 export default class PostScreen extends Component {
-    render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                <Text>Profile screen!</Text>
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+render() {if (!this.props.route.params) {
+      this.props.navigation.navigate("Home");
+    } else if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
             </View>
-        )
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>Spectagram App</Text>
+            </View>
+          </View>
+          <View style={styles.postContainer}>
+            <ScrollView style={styles.postCard}>
+              <Image
+                source={require("../assets/image_1.jpg")}
+                style={styles.image}
+              ></Image>
+
+             
+              <View style={styles.dataContainer}>
+                <View style={styles.titleTextContainer}>
+                  <Text style={styles.postTitleText}>
+                    {this.props.route.params.post.title}
+                  </Text>
+                  <Text style={styles.postAuthorText}>
+                    {this.props.route.params.post.author}
+                  </Text>
+                  <Text style={styles.postAuthorText}>
+                    {this.props.route.params.post.created_on}
+                  </Text>
+                </View>
+                <View style={styles.iconContainer}>
+                  <TouchableOpacity
+                    onPress={() =>
+                      this.initiateTTS(
+                        this.props.route.params.post.title,
+                        this.props.route.params.post.author,
+                        this.props.route.params.post.post,
+                        
+                      )
+                    }
+                  >
+                  
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.postTextContainer}>
+                <Text style={styles.postText}>
+                  {this.props.route.params.post.post}
+                </Text>
+              </View>
+              <View style={styles.actionContainer}>
+                <View style={styles.likeButton}>
+                  <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+                  <Text style={styles.likeText}>12k</Text>
+                </View>
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      );
     }
-}
+  }
+  
